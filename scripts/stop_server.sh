75c268438072fb0bfdda8ba8bef4f8c7ae05d07d
jorgeandresfloreszepeda <69092485+jorgeandresfloreszepeda@users.noreply.github.com>
2025-02-18 18:44:30 -0500

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-# Stop the Apache server
-sudo systemctl stop httpd
+# Check if Apache is installed
+if command -v httpd &> /dev/null; then
+    echo "Apache is installed. Attempting to stop the server..."
+    # Stop the Apache server
+    sudo systemctl stop httpd
+else
+    echo "Apache is not installed."
+fi
